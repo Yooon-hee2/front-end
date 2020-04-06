c36834c1204e99f74749ca02d430b1e970b73b58
Yooon-hee2 <yoooni.kim2@gmail.com>
2020-04-07 01:19:05 +0900

    complete signup, login UI

---
+++ MyApplication/app/src/main/java/com/example/Capstone/SignUpPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.Capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_password)
+    }
+}
