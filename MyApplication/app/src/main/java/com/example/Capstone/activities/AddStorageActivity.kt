3a21cb9217493a500b9b72a06978cc5dcaa367f0
Yooon-hee2 <yoooni.kim2@gmail.com>
2020-04-11 17:01:38 +0900

    complete drawer menu, addstorage activity

---
+++ MyApplication/app/src/main/java/com/example/Capstone/activities/AddStorageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.Capstone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.Capstone.R
+
+class AddStorageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_storage)
+    }
+}
